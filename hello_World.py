ed6096351fb9b4953a29dcb84f51a2d1e1033eb3
Damian Jaramillo <damianjaramillo@gmail.com>
2024-02-29 09:22:25 -0500

    mi segundo  cambio

--- hello_World.py
+++ hello_World.py
@@ -1,6 +1,6 @@
 def welcome_message():
     name = input("Ingrese su nombre: ")
-    print("Bienvenido mi mastergit status: ", name, "al curso de Django y React!")
+    print("Bienvenido mi master man: ", name, "al curso de Django y React!")
 														
 if __name__ == "__main__":
     welcome_message()
